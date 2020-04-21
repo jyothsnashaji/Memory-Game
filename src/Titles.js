06467c6d0a1e397152efd0dd95043d2b20788df8
jyothsnashaji <jyothsnashaji99@gmail.com>
2020-04-21 22:26:06 +0530

    shuffle added

--- src/Titles.js
+++ src/Titles.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-var images=[
+var sources=[
     'https://vignette.wikia.nocookie.net/powerpuff/images/a/a7/Tumblr_lv8big5jK51qizyws.jpg/revision/latest?cb=20120508020045',
     'https://i.pinimg.com/236x/09/66/a3/0966a317bce192cae504b07f3ac3ca86--frozen-elsa-dress-elsa-from-frozen.jpg',
     'https://i.pinimg.com/236x/92/ab/75/92ab75acb818c5621a021acd153839ed--classic-cartoons-vintage-cartoons.jpg',
@@ -12,24 +12,31 @@ var images=[
     'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/220px-SpongeBob_SquarePants_character.svg.png',
 
 ]
+let images;
+
+
 export class Titles extends React.Component{
 
     constructor(props)
     {
         super(props);
         this.updateScore=this.updateScore.bind(this);
+        this.shuffle();
     }
+
     shuffle(){
-        images=images.reverse();
+        images=[]
+        for(var i=0;i<16;++i)
+            images=images.concat([Math.floor(Math.random() * (sources.length-1 ) )]);
     }
     updateScore(e){
         var val=e.target.alt;
         this.props.onClick(val);
-        //shuffle();
+        this.shuffle();
     }
 
     render(){
-        const cards=images.map((src,index)=><li class='images' ><img src={src} alt={index} onClick={this.updateScore}/></li>);
+        const cards=images.map((index)=><li class='images' ><img src={sources[index]} alt={index} onClick={this.updateScore}/></li>);
     return (<ul type='none'>{cards}</ul>);
     }
 }
