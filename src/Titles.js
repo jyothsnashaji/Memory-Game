647688f38a2d1ed01c421c2dceb5587c50e0e3a7
jyothsnashaji <jyothsnashaji99@gmail.com>
2020-04-22 14:07:01 +0530

    styling

--- src/Titles.js
+++ src/Titles.js
@@ -1,51 +1,59 @@
-import React from 'react';
+import React from "react";
 
-
-export const sources=[
-    'https://vignette.wikia.nocookie.net/powerpuff/images/a/a7/Tumblr_lv8big5jK51qizyws.jpg/revision/latest?cb=20120508020045',
-    'https://i.pinimg.com/236x/09/66/a3/0966a317bce192cae504b07f3ac3ca86--frozen-elsa-dress-elsa-from-frozen.jpg',
-    'https://i.pinimg.com/236x/92/ab/75/92ab75acb818c5621a021acd153839ed--classic-cartoons-vintage-cartoons.jpg',
-    'https://i.pinimg.com/236x/aa/ce/42/aace42b8611c1931add59fdc444f940d.jpg',
-    'https://cached.offlinehbpl.hbpl.co.uk/news/OMC/richedit/bean.jpg',
-    'https://i.pinimg.com/236x/16/63/91/166391c7eb1fb456ab655d61a885bae5--tom-and-jerry-cartoon-tom-jerry.jpg',
-    'https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Mickey_Mouse.png/220px-Mickey_Mouse.png',
-    'https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/220px-SpongeBob_SquarePants_character.svg.png',
-
-]
+export const sources = [
+  "https://vignette.wikia.nocookie.net/powerpuff/images/a/a7/Tumblr_lv8big5jK51qizyws.jpg/revision/latest?cb=20120508020045",
+  "https://i.pinimg.com/236x/09/66/a3/0966a317bce192cae504b07f3ac3ca86--frozen-elsa-dress-elsa-from-frozen.jpg",
+  "https://i.pinimg.com/236x/92/ab/75/92ab75acb818c5621a021acd153839ed--classic-cartoons-vintage-cartoons.jpg",
+  "https://i.pinimg.com/236x/aa/ce/42/aace42b8611c1931add59fdc444f940d.jpg",
+  "https://cached.offlinehbpl.hbpl.co.uk/news/OMC/richedit/bean.jpg",
+  "https://i.pinimg.com/236x/16/63/91/166391c7eb1fb456ab655d61a885bae5--tom-and-jerry-cartoon-tom-jerry.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Mickey_Mouse.png/220px-Mickey_Mouse.png",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/220px-SpongeBob_SquarePants_character.svg.png",
+];
 let images;
-let num_of_images_per_row=5;
-export class Titles extends React.Component{
+let num_of_images_per_row = 5;
+export class Titles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateScore = this.updateScore.bind(this);
+    this.shuffle();
+  }
 
-    constructor(props)
-    {
-        super(props);
-        this.updateScore=this.updateScore.bind(this);
-        this.shuffle();
+  shuffle() {
+    images = [];
+    var j = 0;
+    while (j < sources.length) {
+      images = images.concat([j++]);
+      images.splice(
+        Math.floor(Math.random() * (images.length - 1)),
+        0,
+        Math.floor(Math.random() * sources.length)
+      );
+      //  images=  [...Array(15)].map(() => Math.floor(Math.random() * (sources.length)))
     }
 
-    shuffle(){
-        images=[];
-        var j=0;
-        while(j<sources.length)   
-            {images=images.concat([j++]);
-            images.splice(Math.floor(Math.random() * (images.length-1)),0,Math.floor(Math.random() * (sources.length)));   
-           //  images=  [...Array(15)].map(() => Math.floor(Math.random() * (sources.length)))
-            }
-        
-        if(images.length%num_of_images_per_row)
-        {
-            images=images.concat([...Array(num_of_images_per_row-images.length%num_of_images_per_row)].map(() => Math.floor(Math.random() * (sources.length))));
-        }
+    if (images.length % num_of_images_per_row) {
+      images = images.concat(
+        [
+          ...Array(
+            num_of_images_per_row - (images.length % num_of_images_per_row)
+          ),
+        ].map(() => Math.floor(Math.random() * sources.length))
+      );
+    }
+  }
+  updateScore(e) {
+    var val = e.target.alt;
+    this.props.onClick(val);
+    this.shuffle();
+  }
 
-    }
-    updateScore(e){
-        var val=e.target.alt;
-        this.props.onClick(val);
-        this.shuffle();
-    }
-
-    render(){
-        const cards=images.map((index)=><li class='images' ><img src={sources[index]} alt={index} onClick={this.updateScore}/></li>);
-    return (<ul type='none'>{cards}</ul>);
-    }
+  render() {
+    const cards = images.map((index) => (
+      <li class="images">
+        <img src={sources[index]} alt={index} onClick={this.updateScore} />
+      </li>
+    ));
+    return <ul className="titles" type="none">{cards}</ul>;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Titles,sources} from './Titles';
-class Game extends React.Component{
-
-  constructor(props){
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Titles, sources } from "./Titles";
+class Game extends React.Component {
+  constructor(props) {
     super(props);
-    this.handleOnClick=this.handleOnClick.bind(this);
-    this.gameOver=this.gameOver.bind(this);
-    this.state={
-      highest:0,
-      score:0,
-      clicked:[]
-    }
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.gameOver = this.gameOver.bind(this);
+    this.state = {
+      highest: 0,
+      score: 0,
+      clicked: [],
+    };
   }
-  gameOver(){
+  gameOver() {
     this.setState({
-      score:0,
-      clicked:[]
-    })
-
+      score: 0,
+      clicked: [],
+    });
   }
-  handleOnClick(val){
-    let clicked=this.state.clicked;
-    if(clicked.includes(val)) 
-    {
-      alert('YOU LOST!!');
+  handleOnClick(val) {
+    let clicked = this.state.clicked;
+    if (clicked.includes(val)) {
+      alert("YOU LOST!!");
       this.gameOver();
-    }
-    else{
-      if(clicked.length+1===sources.length)
-        {alert("YOU WON!!");
-        this.gameOver();}
-      else
-
-      this.setState({
-        highest: clicked.length +1 >this.state.highest ? clicked.length+1 : this.state.highest,
-        score:this.state.score+1,
-        clicked:clicked.concat([val]),
-      });
+    } else {
+      if (clicked.length + 1 === sources.length) {
+        alert("YOU WON!!");
+        this.gameOver();
+      } else
+        this.setState({
+          highest:
+            clicked.length + 1 > this.state.highest
+              ? clicked.length + 1
+              : this.state.highest,
+          score: this.state.score + 1,
+          clicked: clicked.concat([val]),
+        });
     }
   }
-  render(){
-    return( <div>
+  render() {
+    return (
       <div>
-            <div>
-            <button onClick={this.gameOver}>NEW GAME</button> 
-
-            Score: {this.state.score}     Highest: {this.state.highest}
+        <div>
+          <div className="navbar">
+            <button onClick={this.gameOver}>NEW GAME</button><br></br>
+            <h3>SCORE: {this.state.score} 
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
+            HIGHEST: {this.state.highest}</h3>
+          </div>
         </div>
-        </div>
-      <Titles onClick={this.handleOnClick} />
-
-    </div>);
+        <Titles onClick={this.handleOnClick} />
+      </div>
+    );
   }
 }
-ReactDOM.render(<Game />,document.getElementById('root'));
-
+ReactDOM.render(<Game />, document.getElementById("root"));
